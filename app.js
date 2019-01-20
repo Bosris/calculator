71817722147173757f0ad84a80eafb0a256b10ca
james <jamesallstar21@gmail.com>
2019-01-20 01:11:25 -0500

    starter kit

---
+++ app.js
@@ -0,0 +1,31 @@
+function add(numberOne, numberTwo){
+    var sum = 0;
+    sum = numberOne + numberTwo;
+
+    return sum;
+}
+
+
+function subtract(numberOne, numberTwo){
+    var difference = 0;
+    difference = numberOne - numberTwo;
+
+    return difference;
+}
+
+
+function multiply(numberOne, numberTwo){
+    var product = 0;
+    product = numberOne * numberTwo;
+
+    return product;
+}
+
+function divide(numberOne, numberTwo){
+    var quotient = 0;
+
+    quotient = numberOne / numberTwo;
+
+    return quotient;
+
+}
